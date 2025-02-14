4f528d479ac3cc8929c0c1a77270037fdbf90794
Kyle Ton <kyleton@Kyles-MacBook-Air-M3.local>
2025-02-14 09:12:27 -0800

    set up basic structure

--- backend/src/main/java/com/stockanalysis/controller/TechNewsSentimentAnalsisApplication.java
+++ backend/src/main/java/com/stockanalysis/controller/TechNewsSentimentAnalsisApplication.java
@@ -1,4 +1,4 @@
-package NewsSentimentAnalysis;
+package com.stockanalysis.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
