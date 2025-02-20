ffda717bfd2fde068f2008f83e05bd671f49ed8d
Kyle Ton <kyleton@Kyles-MacBook-Air-M3.local>
2025-02-20 15:35:09 -0800

    working session management for session storage of google user id

--- backend/src/main/java/com/stockanalysis/controller/GenerateNewsController.java
+++ backend/src/main/java/com/stockanalysis/controller/GenerateNewsController.java
@@ -6,20 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpSession;
+
 @RestController
 public class GenerateNewsController {
     
     private final GenerateNewsService generateNewsService;
 
     @Autowired
-    public GenerateNewsController(GenerateNewsService generateNewsService) {
+    public GenerateNewsController(GenerateNewsService generateNewsService, HttpSession httpSession) {
         this.generateNewsService = generateNewsService;
+        
     }
 
     @GetMapping("/getnews")
-    public String generateNews(@RequestParam String symbol) {
+    public String generateNews(@RequestParam String symbol, HttpSession session) {
         System.out.println("Stock symbol: " + symbol);
-        generateNewsService.generateNews(symbol);
+        generateNewsService.generateNews(symbol, session.getAttribute("USER_ID").toString());
         return "News generated successfully for " + symbol;
     }
 
--- backend/src/main/java/com/stockanalysis/service/GoogleAuthenticationService.java
+++ backend/src/main/java/com/stockanalysis/service/GoogleAuthenticationService.java
@@ -5,6 +5,7 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
+
 import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.util.Arrays;
@@ -12,23 +13,30 @@ import java.util.Arrays;
 @Service
 public class GoogleAuthenticationService {
 
+    private String client_id;
+    private NetHttpTransport transport;
+    private GsonFactory gsonFactory;
 
     public GoogleAuthenticationService() {
-        
+        Dotenv dotenv = Dotenv.load();
+        this.client_id = dotenv.get("GOOGLE_CLIENT_ID");
+        this.transport = new NetHttpTransport();
+        this.gsonFactory = GsonFactory.getDefaultInstance();
     }
 
+
     public GoogleIdToken.Payload authenticate(String IDToken) throws IOException {
-        Dotenv dotenv = Dotenv.load();
-        String client_id = dotenv.get("GOOGLE_CLIENT_ID");
-        NetHttpTransport transport = new NetHttpTransport();
-        GsonFactory gsonFactory = GsonFactory.getDefaultInstance();
-        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, gsonFactory)
+        System.out.println("now in authenticate function");
+
+        try {
+            System.out.println(client_id);
+            GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, gsonFactory)
                 .setAudience(Arrays.asList(client_id))
                 .build();
 
-        try {
             GoogleIdToken idToken = verifier.verify(IDToken);
             if (idToken != null) {
+                System.out.println("Payload: " + idToken.getPayload());
                 return idToken.getPayload();
             } else {
                 throw new IOException("Invalid ID token.");
