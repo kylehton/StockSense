32366468d891e51c968d64633e7fdd3b3b3f7955
Kyle Ton <kyleton06@gmail.com>
2025-02-20 10:15:41 -0800

    taking break from working on pipeline flow, retrying setting up google auth

--- backend/src/main/java/com/stockanalysis/models/UserDetails.java
+++ backend/src/main/java/com/stockanalysis/models/UserDetails.java
@@ -1,5 +1,22 @@
 package com.stockanalysis.models;
 
 public class UserDetails {
-    
+    // ThreadLocal to store user ID for the current thread, keeping sessions separate
+    // static so that it is shared across all instances of UserDetails
+    private static final ThreadLocal<String> currentUserId = new ThreadLocal<>();
+
+    public void setUserId(String userId) 
+    {
+        currentUserId.set(userId);
+    }
+
+    public String getUserId() 
+    {
+        return currentUserId.get();
+    }
+
+    public void clear() 
+    {
+        currentUserId.remove();
+    }
 }
