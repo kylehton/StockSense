5027c84556d9dea77f6a3d450554772ab08dd07a
Kyle Ton <kyleton06@gmail.com>
2025-02-25 19:29:52 -0800

    reset

---
+++ backend/src/main/java/com/stockanalysis/config/SessionConfig.java
@@ -0,0 +1,15 @@
+package com.stockanalysis.config;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.MapSessionRepository;
+
+@Configuration
+public class SessionConfig {
+    @Bean
+    public MapSessionRepository sessionRepository() {
+        return new MapSessionRepository(new ConcurrentHashMap<>());
+    }
+}
