c1f06da9905f3eb39de063964b19b211df0dfb5c
Kyle Ton <kyleton06@gmail.com>
2025-04-12 14:08:18 -0700

    frontend is now deployed on vercel. need to iron out backend and db details then will deploy. MVP nearly complete

--- server/src/main/java/com/stockanalysis/config/WebSecurityConfig.java
+++ server/src/main/java/com/stockanalysis/config/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://stock-sense-client.vercel.app/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList(
             "Content-Type", "Authorization", "X-XSRF-TOKEN",
