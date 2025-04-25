90548615f67f22a098ece08f1ad790413d7b8e98
Kyle Ton <kyleton06@gmail.com>
2025-04-25 10:31:22 -0700

    added vercel analytics for data gathering and analysis on webpage data

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   title: "Create Next App",
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         {children}
+        <Analytics />
       </body>
     </html>
   );
